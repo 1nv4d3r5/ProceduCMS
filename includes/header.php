4f63211760474186bcbd3b0c8b5d3d60619088f3
Adrian Mihai <adrian@carve.ro>
2013-03-03 20:31:23 +0200

    added projects page content, complete with jquery gallery functionality

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,13 @@
 		<title>ProceduCMS | Educational Project</title>
 		<link rel="stylesheet" href="css/style.css"/>
 		<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+		<script type="text/javascript" src="js/jquery.fancybox.js?v=2.1.4"></script>
+		<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css?v=2.1.4" media="screen" />
+		<script>
+			$(document).ready(function(){
+				$('.fancybox').fancybox();
+			});
+		</script>
 	</head>
 	<body>
 		<div id="main">
